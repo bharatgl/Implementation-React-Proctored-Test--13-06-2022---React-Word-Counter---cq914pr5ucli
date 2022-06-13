cb168bfe8ecfc9a49c5dea3e817b028913bad2c9
Bharat Goyal <bgoyal1471@gmail.com>
2022-06-13 20:27:04 +0530

    Added functtionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
 import '../styles/App.css';
+import WordCounter from './WordCounter';
 const App = () => {
 
 
   return (
     <div id="main">
+    <WordCounter/>
     </div>
   )
 }
